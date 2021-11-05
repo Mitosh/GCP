79efb87abf74a05d51991300c20d5e07649cf3f6
Mitosh <rouben.sefilian@gmail.com>
2021-11-05 14:11:49 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("../certain-mission-328320-eac570243f2f.json")
+
+  project = "certain-mission-328320"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
